29c656ee24fb675dae9d10c2a11f48f387c545f0
samvProgrammer <santiagoantoniomariscal@gmail.com>
2018-03-17 10:18:34 -0600

    prueba

---
+++ pruebaPinPad/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pruebaPinPad
+{
+    public delegate void resultado(string mensaje);
+    public partial class Form1 : Form
+    {
+        clasePuertos puertos;
+        resultado prueba;
+        public Form1()
+        {
+            InitializeComponent();
+            puertos = new clasePuertos();
+            puertos.prueba3 = mensaje;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string cadena = " @logo1@br@cnn VENTA@br@cnn RETAIL PRUEBAS@cnn (001) RETAIL PRUEBAS@cnn CORREGIDORA 92 @cnn COL. MIGUEL HIDALGO 1A SECCIÃN, DF@br@cnn 2563418 PRUEBAS RETAIL@cnn G374PPPC0 @br@lnn No.Tarjeta: xxxxxxxxxxxx3107@lnn Vence:05/22@br@lnn CREDITO/BBVA BANCOMER/Visa@br@cnb -C-O-M-E-R-C-I-O-@br@lnn APROBADA@lnn IMPORTE @cnb $ 1.00 MXN  @br@lnn Oper.:     000564126@lnn Ref.:      pruebaitijisfj @lnn@lnn AID:       07A0000000032010@lnn Aut.:      546483@lnn  @br@lnn Fecha: 20/05/2017 18:30:00 @br@br@br@cnn @cnn VALIDADO CON FIRMA ELECTRONICA@br@cnn  ME OBLIGO EN LOS TERMINOS DADOS @cnn  AL REVERSO DE ESTE DOCUMENTO@br@logo3br@br@cnn CP-D App.Major.App.Minor.App.Revision@br@bc 000564126@br ";
+            puertos.imprimir(cadena);
+        }
+        public void mensaje(string mensaje) {
+            txtResult.Text = mensaje;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            puertos.informacionPinPad();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            puertos.inicializacionDukpt();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            puertos.mostrarLogo();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            puertos.emisionDeBips("samv");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            puertos.tarjetaConectada();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if(txtMensaje.Text == ""){
+                MessageBox.Show("Se debe mostrar el mensaje");
+                return;
+            }
+
+            puertos.mostrarDisplay(txtMensaje.Text);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            puertos.capturarInformacion();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            puertos.pedirFirmaPanel();
+        }
+    }
+}
---
+++ pruebaPinPad/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pruebaPinPad
+{
+    
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    public class clasePuertos
+    {
+        public resultado prueba3;
+        public string nombrePuerto = string.Empty;
+        SerialPort puerto;
+        public char stx, etx,fs;
+        public clasePuertos() {
+            byte aux = 02;
+            byte aux2 = 03;
+            byte aux3 = 28;
+            stx = Convert.ToChar(aux);
+            etx = Convert.ToChar(aux2);
+            fs = Convert.ToChar(aux3);
+            puerto = new SerialPort();
+            var puertos = SerialPort.GetPortNames();
+            foreach (var item in puertos)
+            {
+                puerto.PortName = item;
+                if (!puerto.IsOpen)
+                {
+                    var variable = puerto.PortName;
+                    break;
+                }
+            }
+            puerto.Open();
+            puerto.BaudRate = 19200;
+            puerto.Parity = Parity.None;
+            puerto.DataBits = 8;
+            puerto.StopBits = StopBits.One;
+        }    
+        public byte LRC(string msg)
+        {
+            byte LRC;
+            LRC = Convert.ToByte(0);
+            var caracteres = msg.ToCharArray();
+
+            foreach (char ch in caracteres)
+            {
+                LRC = Convert.ToByte(LRC ^ Convert.ToByte(ch));
+            }
+            return LRC;
+        }
+
+        internal void imprimir(string inputSome)
+        {
+            string aux = inputSome;
+            var variable = inputSome.ToCharArray();
+            var bien = Encoding.ASCII.GetString(Encoding.ASCII.GetBytes(inputSome));
+            string sndComando = "C59";
+            string mensaje = sndComando + "A" + bien;
+            var variable2 = aux.ToCharArray();
+            string longitud = longitudCadena(mensaje);
+            string output = stx + longitud + mensaje + etx;
+            output += Convert.ToChar(LRC(output));
+            puerto.Write(output); // Va a imprimir pero no necesita un sleep porque eso hace el pinpad.
+            Thread.Sleep(1000);
+            string recibo = puerto.ReadExisting();
+            prueba3(recibo);
+        }
+        public void informacionPinPad() {
+            if (!puerto.IsOpen)
+            {
+                puerto.Open();
+            }
+            string ComandoSnd = "C56";
+            string longitudMensaje = "LLL"; //Se puede mandar de esta manera con puro LLL
+            string formatoMensaje = "A00";
+            string mensaje = "00";
+            string cadenaAux = stx + longitudMensaje + ComandoSnd+"AABOUT" + etx;
+            cadenaAux = cadenaAux +Convert.ToChar(LRC(cadenaAux));
+            puerto.Write(cadenaAux);
+            Thread.Sleep(1000);
+            string aux = puerto.ReadExisting();
+            string[] arreglo = {"Marca","Modelo","Numero serie","version","Bandera EMV "};
+            prueba3(aux);
+        }
+
+        private string voucherPinPad(string mensaje)
+        {
+            string ComandoSnd = "C59";
+            string longitudMensaje = "LLL"; //Se puede mandar de esta manera con puro LLL
+            string formatoMensaje = "A@lnn";
+            string saltosLinea = "@br@br@br@br@br@br";
+            string cadenaAux = stx + longitudMensaje +ComandoSnd+ formatoMensaje + mensaje +saltosLinea+ etx;
+            cadenaAux = cadenaAux + Convert.ToChar(LRC(cadenaAux));
+            
+            return cadenaAux;
+        }
+
+        internal void inicializacionDukpt()
+        {
+            DateTime fecha1 = DateTime.Now;
+            string fecha = fecha1.ToString("ddMMyy");
+            string hora2 = fecha1.ToString("HHmm");
+            string sndComando = "C91";
+            string mensaje = sndComando + "A" +fecha+fs+"B"+hora2;
+            string longitud = longitudCadena(mensaje);
+            string output = stx + longitud + mensaje + etx;
+            output += Convert.ToChar(LRC(output));
+            puerto.Write(output);
+            Thread.Sleep(1500);
+            string resultado = puerto.ReadExisting();
+            prueba3(resultado);   
+        }
+
+        public void emisionDeBips(string segundo) {
+            string sndComando = "C80";
+            string mensaje = sndComando+"A4";
+            string longitud = longitudCadena(mensaje);
+            string output = stx + longitud + mensaje + etx;
+            output += Convert.ToChar(LRC(output));
+            puerto.Write(output);
+            Thread.Sleep(1500);
+            string resultado = puerto.ReadExisting();
+            prueba3(resultado);
+        }
+
+        public void mostrarLogo() {
+            // STXLLLC58ABANCOETXLRC
+            string ComandoSnd = "C58";
+            string mensaje = "ASANTANDER";
+            
+            mensaje = stx + "015" + ComandoSnd+ mensaje + etx;
+            mensaje += Convert.ToChar(LRC(mensaje));
+            puerto.Write(mensaje);
+            Thread.Sleep(2000);
+            string result = puerto.ReadExisting();
+            prueba3(result);
+        }
+
+        public bool tarjetaConectada() {
+            string sndComando = "C82";
+            string mensaje = sndComando;
+            string longitud = longitudCadena(mensaje);
+            string output = stx + longitud + mensaje + etx;
+            output += Convert.ToChar(LRC(output));
+            puerto.Write(output);
+            Thread.Sleep(1500);
+            string resultado = puerto.ReadExisting();
+            return true;   
+        }
+
+        public void mostrarDisplay(string mensajeaux) {
+            string sndComando = "C50";
+            string mensaje = sndComando+"A"+mensajeaux;
+            string longitud = longitudCadena(mensaje);
+            string output = stx + longitud + mensaje + etx;
+            output += Convert.ToChar(LRC(output));
+            puerto.Write(output);
+        }
+
+        public void capturarInformacion() {
+
+            string sndComando = "C60";
+            string mensaje = sndComando + "ACaptureInformacion"+fs+"B5";
+            string longitud = longitudCadena(mensaje);
+            string output = stx + longitud + mensaje + etx;
+            output += Convert.ToChar(LRC(output));
+            puerto.Write(output);
+            string auxiliar = puerto.ReadExisting();
+        }
+
+        public void pedirFirmaPanel() { 
+         string sndComando = "C61";
+            string mensaje = sndComando + "A00"+fs+"BFirma panel"+fs+"C15"+fs+"DPNG";
+            string longitud = longitudCadena(mensaje);
+            string output = stx + longitud + mensaje + etx;
+            output += Convert.ToChar(LRC(output));
+            puerto.Write(output);
+            string auxiliar = puerto.ReadExisting();
+        }
+
+        public string longitudCadena(string cadena) {
+            int longitud = cadena.Length;
+            string aux = "";
+            if (longitud < 10)
+            {
+                aux = "00" + Convert.ToString(longitud);
+            }
+            else if (longitud <  100)
+            {
+                aux = "0" + Convert.ToString(longitud);
+            }
+            else {
+                aux = Convert.ToString(longitud);
+            }
+            return aux;
+        }
+
+       
+    }
+}
